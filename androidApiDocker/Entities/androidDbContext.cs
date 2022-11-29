3353e396fc981b89b8ad3acfa5fa63515fe68174
shkim4548 <73636884+shkim4548@users.noreply.github.com>
2022-11-29 13:54:54 +0900

    connectionString, sql add

--- androidApiDocker/Entities/androidDbContext.cs
+++ androidApiDocker/Entities/androidDbContext.cs
@@ -27,7 +27,7 @@ namespace androidApiDocker.Entities
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=Blizard5000@;database=androidDb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.29-mysql"));
+                optionsBuilder.UseMySql("server=mysql-container;port=3306;user=root;password=7426;database=androidDb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.29-mysql"));
             }
         }
 
--- androidApiDocker/Program.cs
+++ androidApiDocker/Program.cs
@@ -1,12 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-int hostPort = Int16.Parse(builder.Configuration["Host:Port"]);
-
+//int hostPort = Int16.Parse(builder.Configuration["Host:Port"]);
+/*
 builder.WebHost.UseKestrel(options =>
 {
     options.Listen(System.Net.IPAddress.Any, hostPort);
 });
-
+*/
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
